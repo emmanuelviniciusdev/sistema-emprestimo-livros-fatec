8238570e9673b7c49c8b2262e18401d839749c77
emmanuelviniciusdev <emmanuelviniciusdev@gmail.com>
2022-11-05 18:01:07 -0300

    Adiciona tabelas: 'tb_livro', 'tb_emprestimo'

--- html/logs/criacoes.sql
+++ html/logs/criacoes.sql
@@ -22,3 +22,37 @@ ALTER TABLE `tb_usuario`
 ALTER TABLE `tb_usuario`
   MODIFY `id` int NOT NULL AUTO_INCREMENT;
 COMMIT;
+
+-- Cria 'tb_livro'
+CREATE TABLE `tb_livro` (
+  `id` int NOT NULL,
+  `autor` varchar(100) DEFAULT NULL,
+  `titulo` varchar(255) DEFAULT NULL,
+  `area` varchar(40) DEFAULT NULL,
+  `ano` smallint DEFAULT NULL,
+  `tombo` char(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+ALTER TABLE `tb_livro`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `tb_livro`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+-- Cria 'tb_emprestimo'
+CREATE TABLE `tb_emprestimo` (
+  `id_livro` int NOT NULL,
+  `id_usuario` int NOT NULL,
+  `data_emprestimo` datetime NOT NULL,
+  `data_devolucao` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+ALTER TABLE `tb_emprestimo`
+  ADD PRIMARY KEY (`id_livro`,`id_usuario`),
+  ADD KEY `tb_emprestimo_ibfk_2` (`id_usuario`);
+
+ALTER TABLE `tb_emprestimo`
+  ADD CONSTRAINT `tb_emprestimo_ibfk_1` FOREIGN KEY (`id_livro`) REFERENCES `tb_livro` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `tb_emprestimo_ibfk_2` FOREIGN KEY (`id_usuario`) REFERENCES `tb_usuario` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+COMMIT;
